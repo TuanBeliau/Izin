ad317e18a156b0f0387e43ad9543e3bc67518efb
TuanBeliau <161784517+TuanBeliau@users.noreply.github.com>
2024-03-22 08:44:11 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
-session_start(); //untuk memulai
-$_SESSION = null; //MENGHAPUS Data
-session_destroy();//untuk meng header('Location: login.php');
-header('Location: login.php');
+  session_start(); //untuk memulai
+  $_SESSION = null; //MENGHAPUS Data
+  session_destroy();//untuk meng header('Location: login.php');
+  header('Location: login.php');
 ?>
