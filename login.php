034e2a2a82f154e5935c14efc3ee8d52d2ea700b
TuanBeliau <161784517+TuanBeliau@users.noreply.github.com>
2024-03-22 07:41:33 +0700

    Update login.php

--- login.php
+++ login.php
@@ -24,14 +24,14 @@
                   <img src="assets/images/random/black.jp" width="180" alt="">
                 </a>
                 <p class="text-center">Your Social Campaigns</p>
-                <form>
+                <form action='prolog.php' method='post' >
                   <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Username</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   </div>
                   <div class="mb-4">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                   </div>
                   <div class="d-flex align-items-center justify-content-between mb-4">
                     <!-- <div class="form-check">
