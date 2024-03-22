9eb04235a9602c709e9d92525dac497b604d9479
TuanBeliau <161784517+TuanBeliau@users.noreply.github.com>
2024-03-22 07:31:44 +0700

    Create prolog.php

---
+++ prolog.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'config/connect.php'; // Menghubungkan ke file koneksi.php untuk membuat ko
+
+$username = $_POST['username'];
+$password = $_POST['password'];// Mengambil nilai username dari form $_POST['password']; // Mengambil nilai password dari form
+
+$query = "SELECT * FROM siswa WHERE username='$username' AND password $password $result = mysqli_query($is_connect, $query); // Menjalankan query pada database
+
+if (mysqli_num_rows($result) > 0) { // Jika query menghasilkan satu atau lebih bari
+
+session_start(); // Memulai sesi
+
+$_SESSION['username'] = $username; // Menyimpan username dalam sesi header('Location: index.php'); // Mengalihkan ke halaman
+
+} else {
+
+echo 'Login gagal! Username atau password salah.';
+}
+?>
