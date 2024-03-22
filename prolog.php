ac3ca0575ad15c065e7372626773c140a9120b58
TuanBeliau <161784517+TuanBeliau@users.noreply.github.com>
2024-03-22 08:43:31 +0700

    Update prolog.php

--- prolog.php
+++ prolog.php
@@ -5,16 +5,16 @@ include 'config/connect.php'; // Menghubungkan ke file koneksi.php untuk membuat
 $username = $_POST['username'];
 $password = $_POST['password'];// Mengambil nilai username dari form $_POST['password']; // Mengambil nilai password dari form
 
-$query = "SELECT * FROM siswa WHERE username='$username' AND password $password $result = mysqli_query($is_connect, $query); // Menjalankan query pada database
+$query = "SELECT * FROM siswa WHERE username='$username' AND password='$password';
+$result = mysqli_query($is_connect, $query); // Menjalankan query pada database
 
 if (mysqli_num_rows($result) > 0) { // Jika query menghasilkan satu atau lebih bari
-
-session_start(); // Memulai sesi
-
-$_SESSION['username'] = $username; // Menyimpan username dalam sesi header('Location: index.php'); // Mengalihkan ke halaman
-
-} else {
-
-echo 'Login gagal! Username atau password salah.';
+  session_start(); // Memulai sesi
+  $_SESSION['username'] = $username; // Menyimpan username dalam sesi header('Location: index.php'); // Mengalihkan ke halaman
+  $_SESSION['id'] = $data['id'];
+  header('Location: index.php');
+} 
+else {
+  echo 'Login gagal! Username atau password salah.';
 }
 ?>
